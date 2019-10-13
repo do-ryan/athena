3a60ee4bad2e8c527c7acc0f3c1c6df1e35e8074
Ryan Do <ryan.jg.do@gmail.com>
2019-10-13 13:21:09 -0400

    distinct substrings suffix array construction

--- PEG/distinct_substrings.py
+++ PEG/distinct_substrings.py
@@ -26,3 +26,32 @@ Sample Output
 7
 4
 """
+#import pdb; pdb.set_trace()
+
+def inp():
+    N = int(input())
+    strings = [input() for i in range(N)]
+    return strings
+
+def string_to_suffix_array(string):
+    char_lexi_order = [ord(char) for char in string]
+    suffix_order = suffix_sort_recurse(order_list=char_lexi_order, order_list_range=1)
+    suffix_array = [string[i:]  for i in range(-len(string), 0)]
+    sorted_suffix_array = [0]*len(suffix_array)
+    for i, order in enumerate(suffix_order):
+        sorted_suffix_array[order] = suffix_array[i]
+    return sorted_suffix_array
+
+def suffix_sort_recurse(order_list: list, order_list_range: int):
+    if order_list_range > len(order_list) or len(set(order_list)) == len(order_list):  # first condition redundant because first condition guarantees second condition
+        return order_list
+    next_order_list = [(order, order_list[i+order_list_range]) if (i+order_list_range < len(order_list)) else (order, -1) for i, order in enumerate(order_list)]
+    sort_index = {tup: i for i, tup in enumerate(sorted(next_order_list))}
+    next_order_list = [sort_index[tup] for tup in next_order_list]
+    return suffix_sort_recurse(next_order_list, order_list_range*2)
+
+
+if __name__ == "__main__":
+    strings = inp()
+    for string in strings:
+        print(string_to_suffix_array(string))
