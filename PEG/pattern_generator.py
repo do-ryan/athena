892c565233bf5ceeec5b5ba94c003c5301092bff
Ryan Do <ryan.jg.do@gmail.com>
2019-10-18 09:15:16 -0400

    pattern generator ac

--- PEG/pattern_generator.py
+++ PEG/pattern_generator.py
@@ -29,6 +29,21 @@ The bit patterns are
 0101
 0011
 """
+def inp():
+    n_pairs = int(input())
+    pairs = []
+    for _ in range(n_pairs):
+        pairs.append((int(tok) for tok in input().split(' ')))
+    return pairs
+
+def find_n_length_with_k_ones(n, k, bit_patterns=[]):
+    if k == 0:
+        return ['0'*n]
+    elif n == k:
+        return ['1'*n]
+    else:
+        return ['1' + pat for pat in find_n_length_with_k_ones(n-1, k-1)] + ['0' + pat for pat in find_n_length_with_k_ones(n-1, k)]
+
 
 if __name__ == "__main__":
 
@@ -40,3 +55,11 @@ if __name__ == "__main__":
         in the scrambling process, we see all the different possible letter combinations, including same combinations for k with different orders. We divide n! by k! to eliminate all the different permutations of the same combinations. We divide again by (n-k)! to eliminate all the different permutations of the remaining letters (last n-k digits). As a result, the number of bit pattern combinations are n!/k!(n-k)!
         - to find all combinations, recursively try both values for each digit and return when k 0s are encountered. O(2^n)
         - O(min(n^k, n^(n-k)) solution: k loops of n times each and and find all distinct choices of n in k. the choices define where to place the 1s. skip when duplicate location is encountered. big memory use however possibly... 
+    """
+    n_ks = inp()
+    for n, k in n_ks:
+        print('The bit patterns are')
+        for pattern in find_n_length_with_k_ones(n, k):
+            print(pattern)
+        print()
+    pass
