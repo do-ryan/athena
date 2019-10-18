adede302f5394f1d77b47c7e06838fa932d5478f
Ryan Do <ryan.jg.do@gmail.com>
2019-10-18 09:24:13 -0400

    new problem

---
+++ PEG/directed_acyclic_graph.py
@@ -0,0 +1,22 @@
+'''
+Given a directed graph, output YES if it is acyclic, and NO otherwise.
+Input
+As usual.
+N ≤ 1000.
+Output
+YES or NO.
+Sample Input
+3
+0 1 0
+0 0 1
+1 0 0
+Sample Output
+NO
+Sample Input 2
+3
+0 1 0
+0 0 1
+0 0 0
+Sample Output 2
+YES
+'''
