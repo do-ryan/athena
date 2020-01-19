3b4db744f0d7bf2499833f9a461f0b04e75d94a1
Ryan Do <ryan.jg.do@gmail.com>
2020-01-18 21:33:37 -0500

    206_Reverse_Linked_List.pyAC recursive

---
+++ LeetCode/easy/206_Reverse_Linked_List.py
@@ -0,0 +1,29 @@
+"""
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    root = None
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return
+        if head.next is None:
+            global root
+            root = head
+            return root
+        self.reverseList(head.next)
+        if head.next.next is None:
+            head.next.next = head
+            head.next = None
+        return root
